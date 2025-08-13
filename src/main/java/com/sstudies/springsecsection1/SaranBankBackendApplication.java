5829d26203c946c62c0335d73b3660eb5143cee5
sarankumarstudies <159512996+sarankumarstudies@users.noreply.github.com>
2025-08-13 10:00:05 +0530

    Add files via upload

---
+++ src/main/java/com/sstudies/springsecsection1/SaranBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sstudies.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaranBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaranBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sstudies/springsecsection1/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.sstudies.springsecsection1.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping ("/api")
+@RestController
+public class WelcomeController {
+
+@GetMapping("/welcomeMessage")
+    public String welcomeNotice () {
+        return "Welcome to the saran bank application";
+    }
+}
